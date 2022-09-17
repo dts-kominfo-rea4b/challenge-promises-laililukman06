52482541cf71cb4d0b6f4405aa4eb3fc2b4e8a56
laili_lukman <laili.lukman@gmail.com>
2022-09-17 20:04:09 +0700

    Add Promise

--- promise.js
+++ promise.js
@@ -1,7 +1,24 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  return new Promise(async(resolve) => {
+    let jumlahEmosiFilm = 0;
+
+    const dataTheaterIXX = await promiseTheaterIXX();
+    dataTheaterIXX.forEach((film) => {
+      if (film.hasil === emosi) jumlahEmosiFilm++;
+    });
+
+    const dataTheaterVGC = await promiseTheaterVGC();
+    dataTheaterVGC.forEach((film)=>{
+      if (film.hasil === emosi) jumlahEmosiFilm++;
+    });
+
+    resolve(jumlahEmosiFilm);
+  })
+
+};
 
 module.exports = {
   promiseOutput,
